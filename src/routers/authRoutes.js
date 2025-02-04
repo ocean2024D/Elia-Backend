a2f9e40d3ca20fcbfa4f3aa6fcd6df7642196a20
derya <lacin>
2025-02-04 20:43:21 +0100

    delete-User added

--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const { login, register, me } = require("../controllers/authController")
+const { login, register, me,deleteUser } = require("../controllers/authController")
 const authValidation = require("../middelwares/validations/authValidation")
 const {tokenCheck} = require("../middelwares/auth")
 
@@ -9,5 +9,5 @@ router.post("/register", register, authValidation.register, register)
 
 router.get("/me", tokenCheck, me)
 
-
+router.delete("/user/:id", deleteUser);
 module.exports = router
