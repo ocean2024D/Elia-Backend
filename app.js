569a10faa7445767ef61f1dc85afd9c90958bdd8
Kevin Selvais <kalzone99@gmail.com>
2025-02-14 10:11:27 +0100

    troubleshooting app.js

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-
-require("express-async-errors")
-const express = require("express")
-const app = express()
-require("dotenv").config()
-require("./src/db/dbConnection")
-const port = process.env.PORT || 5001
-const authRoutes = require("./src/routers/authRoutes")
-const dutyExchangeRoutes = require("./src/routers/RequestRoutes")
-const dutyRoutes = require("./src/routers/dutyRoutes")
-const dutySchedulerRoutes = require("./src/routers/dutySchedulerRoutes")
-const errorHandlerMiddleware = require("./src/middelwares/errorHandler")
-const cors = require("cors")
+require("express-async-errors");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("./src/db/dbConnection");
+const port = process.env.PORT || 5001;
+const authRoutes = require("./src/routers/authRoutes");
+const dutyExchangeRoutes = require("./src/routers/RequestRoutes");
+const dutyRoutes = require("./src/routers/dutyRoutes");
+const dutySchedulerRoutes = require("./src/routers/dutySchedulerRoutes");
+const errorHandlerMiddleware = require("./src/middelwares/errorHandler");
+const cors = require("cors");
 //_____________________________________________________
 // dans le fichier app.js
 // const moment = require("moment-timezone")
 // moment.tz.setDefault("Europe/Brussels")
 
-
-// // dans le dossier utils // 
+// // dans le dossier utils //
 // import moment from "moment-timezone";
 
 // moment.tz.setDefault("Europe/Brussels");
@@ -26,16 +24,13 @@ const cors = require("cors")
 //     return moment().format("YYYY-MM-DD HH:mm:ss");
 // };
 
-
 // // import { getBrusselsTime } from "App/Utils/DateHelper";
 
 // Route.get("/", async () => {
 //     return { heure: getBrusselsTime() };
 // });
 
-
 //______________________________________________________
-
 
 //Middelwares
 app.use(express.json());
@@ -45,13 +40,15 @@ app.use(
 );
 
 // CORS Middleware
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-app.use("/api", router);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 // Define Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/dutyExchange", dutyExchangeRoutes);
