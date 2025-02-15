cb810ec617b11c9b31837d2e636f3621dc72ce80
derya <lacin>
2025-02-15 19:27:09 +0100

    getDutyExchangesByUserId created

--- src/routers/requestRoutes.js
+++ src/routers/requestRoutes.js
@@ -4,6 +4,7 @@ const {
   getAllDutyExchanges,
   getDutyExchangeById,
   acceptDutyRequest,
+  getDutyExchangesByUserId,
   rejectDutyRequest,
 } = require("../controllers/requestController");
 
@@ -12,12 +13,15 @@ const {
 router.post("/", createDutyExchange);
 
 
-router.get("/", getAllDutyExchanges);
+router.get("/", getAllDutyExchanges);//get all exchanges //Endponints
+//  http://localhost:8080/api/dutyExchange?status=accepted
+// http://localhost:8080/api/dutyExchange?status=rejected
+// http://localhost:8080/api/dutyExchange?status=pending
 
 
-router.get("/:exchangeId", getDutyExchangeById);
+router.get("/:exchangeId", getDutyExchangeById); //by dutyExchangeID
 
-
+router.get("/:user/:userId", getDutyExchangesByUserId);//By user Id retrieving DutiesExchanges
 router.post('/accept/:id', acceptDutyRequest); // newObjectId we write in postman  Post  http://localhost:8080/api/accept/67a3dddd65d159d137aa4522,
 // // Postman in Raw{  "acceptingUserId": "67a20681c3d78ecd7aa311e0"}
 
