2695bc3f2995922c0883cab870c1bf403cafd7a5
Kevin Selvais <kalzone99@gmail.com>
2025-02-15 15:33:50 +0100

    changes and new routes and controllers for fetch users by zones and user by id

--- src/models/dutyModel.js
+++ src/models/dutyModel.js
@@ -1,42 +1,49 @@
 const { number } = require("joi");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const DutySchema = new mongoose.Schema({
-  weekNumber: {type:Number},
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
-  username:{type: String},
-  assignedUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  zone : { type: String, required: true },
-  status: {
-    type: String,
-    enum: ["guard", "working", "sick", "vacation", "others"], 
-    default: "guard"
-},
-  
-  days: [
-    {
+const DutySchema = new mongoose.Schema(
+  {
+    weekNumber: { type: Number },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    username: { type: String },
+    assignedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    zone: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["guard", "working", "sick", "vacation", "others"],
+      default: "guard",
+    },
+
+    days: [
+      {
         date: { type: Date, required: true },
         startTime: { type: Date, required: false },
         endTime: { type: Date, required: false },
 
-        assignedUser: { 
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: 'User' 
+        assignedUser: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "user",
         },
 
         exchangeDetails: {
-            acceptingUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-            exchangeStart: { type: Date },
-            exchangeEnd: { type: Date },
-          },
-        status: {
-            type: String,
-            enum: ["guard", "working", "sick", "vacation", "others"], 
-            default: "guard"
+          acceptingUser: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+          exchangeStart: { type: Date },
+          exchangeEnd: { type: Date },
         },
-    }
-],
-}, {collection: "duties", timestamps: true})
+        status: {
+          type: String,
+          enum: ["guard", "working", "sick", "vacation", "others"],
+          default: "guard",
+        },
+      },
+    ],
+  },
+  { collection: "duties", timestamps: true }
+);
 
-module.exports = mongoose.model('Duty', DutySchema);
+module.exports = mongoose.model("Duty", DutySchema);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -36,6 +36,6 @@ const userSchema = new mongoose.Schema(
   { collection: "users", timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
